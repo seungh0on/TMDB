9ec1af1a7a03203178cb8504a2b7a4f387f32fcb
한승훈 <seunghoon526@naver.com>
2024-03-28 18:17:28 +0900

    commit1

---
+++ react-basic-demo/src/movieDummy.js
@@ -0,0 +1,404 @@
+export const dummy ={
+    "page": 1,
+    "results": [
+        {
+            "adult": false,
+            "backdrop_path": "/1XDDXPXGiI8id7MrUxK36ke7gkX.jpg",
+            "genre_ids": [
+                28,
+                12,
+                16,
+                35,
+                10751
+            ],
+            "id": 1011985,
+            "original_language": "en",
+            "original_title": "Kung Fu Panda 4",
+            "overview": "Po is gearing up to become the spiritual leader of his Valley of Peace, but also needs someone to take his place as Dragon Warrior. As such, he will train a new kung fu practitioner for the spot and will encounter a villain called the Chameleon who conjures villains from the past.",
+            "popularity": 4026.558,
+            "poster_path": "/kDp1vUBnMpe8ak4rjgl3cLELqjU.jpg",
+            "release_date": "2024-04-10",
+            "title": "Kung Fu Panda 4",
+            "video": false,
+            "vote_average": 6.958,
+            "vote_count": 343
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/zAepSrO99owYwQqi0QG2AS0dHXw.jpg",
+            "genre_ids": [
+                28,
+                14
+            ],
+            "id": 634492,
+            "original_language": "en",
+            "original_title": "Madame Web",
+            "overview": "Forced to confront revelations about her past, paramedic Cassandra Webb forges a relationship with three young women destined for powerful futures...if they can all survive a deadly present.",
+            "popularity": 2212.848,
+            "poster_path": "/rULWuutDcN5NvtiZi4FRPzRYWSh.jpg",
+            "release_date": "2024-03-13",
+            "title": "Madame Web",
+            "video": false,
+            "vote_average": 5.615,
+            "vote_count": 788
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/deLWkOLZmBNkm8p16igfapQyqeq.jpg",
+            "genre_ids": [
+                14,
+                28,
+                12
+            ],
+            "id": 763215,
+            "original_language": "en",
+            "original_title": "Damsel",
+            "overview": "A young woman's marriage to a charming prince turns into a fierce fight for survival when she's offered up as a sacrifice to a fire-breathing dragon.",
+            "popularity": 1411.052,
+            "poster_path": "/sMp34cNKjIb18UBOCoAv4DpCxwY.jpg",
+            "release_date": "2024-03-08",
+            "title": "Damsel",
+            "video": false,
+            "vote_average": 7.2,
+            "vote_count": 1204
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/7CYeSazkpkWS1znWM11ucA2L6CR.jpg",
+            "genre_ids": [
+                28,
+                878,
+                12
+            ],
+            "id": 823464,
+            "original_language": "en",
+            "original_title": "Godzilla x Kong: The New Empire",
+            "overview": "Following their explosive showdown, Godzilla and Kong must reunite against a colossal undiscovered threat hidden within our world, challenging their very existence – and our own.",
+            "popularity": 1297.198,
+            "poster_path": "/bQ2ywkchIiaKLSEaMrcT6e29f91.jpg",
+            "release_date": "2024-03-27",
+            "title": "Godzilla x Kong: The New Empire",
+            "video": false,
+            "vote_average": 8,
+            "vote_count": 28
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/oFAukXiMPrwLpbulGmB5suEZlrm.jpg",
+            "genre_ids": [
+                28,
+                12,
+                878,
+                14,
+                18
+            ],
+            "id": 624091,
+            "original_language": "id",
+            "original_title": "Sri Asih",
+            "overview": "Alana discover the truth about her origin: she’s not an ordinary human being. She may be the gift for humanity and become its protector as Sri Asih. Or a destruction, if she can’t control her anger.",
+            "popularity": 1102.39,
+            "poster_path": "/wShcJSKMFg1Dy1yq7kEZuay6pLS.jpg",
+            "release_date": "2023-07-01",
+            "title": "Sri Asih",
+            "video": false,
+            "vote_average": 6.423,
+            "vote_count": 39
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/mDeUmPe4MF35WWlAqj4QFX5UauJ.jpg",
+            "genre_ids": [
+                28,
+                27,
+                53
+            ],
+            "id": 1096197,
+            "original_language": "pt",
+            "original_title": "No Way Up",
+            "overview": "Characters from different backgrounds are thrown together when the plane they're travelling on crashes into the Pacific Ocean. A nightmare fight for survival ensues with the air supply running out and dangers creeping in from all sides.",
+            "popularity": 968.007,
+            "poster_path": "/hu40Uxp9WtpL34jv3zyWLb5zEVY.jpg",
+            "release_date": "2024-01-18",
+            "title": "No Way Up",
+            "video": false,
+            "vote_average": 6.164,
+            "vote_count": 284
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/mEoIDEiePnYj178H9znzbl9zvky.jpg",
+            "genre_ids": [
+                28
+            ],
+            "id": 1006540,
+            "original_language": "en",
+            "original_title": "Bullet Train Down",
+            "overview": "On its maiden run, the world's fastest bullet train is rigged with a bomb that will explode if it dips below 200 mph.",
+            "popularity": 832.704,
+            "poster_path": "/5a7cocgyVuFjYV71neDIGVzD6Yq.jpg",
+            "release_date": "2022-08-01",
+            "title": "Bullet Train Down",
+            "video": false,
+            "vote_average": 4.972,
+            "vote_count": 18
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/u0P5drNyTrZoGyJONPcZGbv1mNP.jpg",
+            "genre_ids": [
+                28,
+                53
+            ],
+            "id": 1124127,
+            "original_language": "en",
+            "original_title": "Air Force One Down",
+            "overview": "On her first assignment aboard Air Force One, a rookie Secret Service agent faces the ultimate test when terrorists hijack the plane, intent on derailing a pivotal energy deal. With the President's life on the line and a global crisis at stake, her bravery and skills are pushed to the limit in a relentless battle that could change the course of history.",
+            "popularity": 810.393,
+            "poster_path": "/nKPoSD4pZ3s3CJ9g3cqWRcQ41TC.jpg",
+            "release_date": "2024-02-09",
+            "title": "Air Force One Down",
+            "video": false,
+            "vote_average": 6.291,
+            "vote_count": 43
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/fFdlyeC8qPROUqM8ydR9INXAG17.jpg",
+            "genre_ids": [
+                28,
+                53
+            ],
+            "id": 359410,
+            "original_language": "en",
+            "original_title": "Road House",
+            "overview": "Ex-UFC fighter Dalton takes a job as a bouncer at a Florida Keys roadhouse, only to discover that this paradise is not all it seems.",
+            "popularity": 1307.152,
+            "poster_path": "/bXi6IQiQDHD00JFio5ZSZOeRSBh.jpg",
+            "release_date": "2024-03-08",
+            "title": "Road House",
+            "video": false,
+            "vote_average": 7.3,
+            "vote_count": 599
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/meyhnvssZOPPjud4F1CjOb4snET.jpg",
+            "genre_ids": [
+                16,
+                28,
+                12,
+                35,
+                10751
+            ],
+            "id": 940551,
+            "original_language": "en",
+            "original_title": "Migration",
+            "overview": "After a migrating duck family alights on their pond with thrilling tales of far-flung places, the Mallard family embarks on a family road trip, from New England, to New York City, to tropical Jamaica.",
+            "popularity": 847.528,
+            "poster_path": "/ldfCF9RhR40mppkzmftxapaHeTo.jpg",
+            "release_date": "2024-01-10",
+            "title": "Migration",
+            "video": false,
+            "vote_average": 7.6,
+            "vote_count": 1006
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/xvk5AhfhgQcTuaCQyq3XqAnhEma.jpg",
+            "genre_ids": [
+                28,
+                12,
+                35
+            ],
+            "id": 848538,
+            "original_language": "en",
+            "original_title": "Argylle",
+            "overview": "When the plots of reclusive author Elly Conway's fictional espionage novels begin to mirror the covert actions of a real-life spy organization, quiet evenings at home become a thing of the past. Accompanied by her cat Alfie and Aiden, a cat-allergic spy, Elly races across the world to stay one step ahead of the killers as the line between Conway's fictional world and her real one begins to blur.",
+            "popularity": 743.424,
+            "poster_path": "/95VlSEfLMqeX36UVcHJuNlWEpwf.jpg",
+            "release_date": "2024-02-07",
+            "title": "Argylle",
+            "video": false,
+            "vote_average": 6.156,
+            "vote_count": 707
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/qVrS8bu1B7G1tgLTlCZQi4CFsTh.jpg",
+            "genre_ids": [
+                28,
+                53,
+                10752
+            ],
+            "id": 969492,
+            "original_language": "en",
+            "original_title": "Land of Bad",
+            "overview": "When a Delta Force special ops mission goes terribly wrong, Air Force drone pilot Reaper has 48 hours to remedy what has devolved into a wild rescue operation. With no weapons and no communication other than the drone above, the ground mission suddenly becomes a full-scale battle when the team is discovered by the enemy.",
+            "popularity": 734.629,
+            "poster_path": "/h3jYanWMEJq6JJsCopy1h7cT2Hs.jpg",
+            "release_date": "2024-03-13",
+            "title": "Land of Bad",
+            "video": false,
+            "vote_average": 7.117,
+            "vote_count": 422
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/87IVlclAfWL6mdicU1DDuxdwXwe.jpg",
+            "genre_ids": [
+                878,
+                12
+            ],
+            "id": 693134,
+            "original_language": "en",
+            "original_title": "Dune: Part Two",
+            "overview": "Follow the mythic journey of Paul Atreides as he unites with Chani and the Fremen while on a path of revenge against the conspirators who destroyed his family. Facing a choice between the love of his life and the fate of the known universe, Paul endeavors to prevent a terrible future only he can foresee.",
+            "popularity": 739.178,
+            "poster_path": "/8b8R8l88Qje9dn9OE8PY05Nxl1X.jpg",
+            "release_date": "2024-02-28",
+            "title": "Dune: Part Two",
+            "video": false,
+            "vote_average": 8.392,
+            "vote_count": 2156
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/47olX0FCvUCfAqlp8cK0O5fKLav.jpg",
+            "genre_ids": [
+                16,
+                35,
+                878
+            ],
+            "id": 1239251,
+            "original_language": "en",
+            "original_title": "Megamind vs. the Doom Syndicate",
+            "overview": "Megamind's former villain team, The Doom Syndicate, has returned. Our newly crowned blue hero must now keep up evil appearances until he can assemble his friends (Roxanne, Ol' Chum and Keiko) to stop his former evil teammates from launching Metro City to the Moon.",
+            "popularity": 696.013,
+            "poster_path": "/yRZfiG1QpRkBc7fAmxfcR7Md5EC.jpg",
+            "release_date": "2024-03-01",
+            "title": "Megamind vs. the Doom Syndicate",
+            "video": false,
+            "vote_average": 5.7,
+            "vote_count": 137
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/ekRp1sEA8pnuzVHQkUESTgNSKdW.jpg",
+            "genre_ids": [
+                878,
+                28,
+                80
+            ],
+            "id": 932420,
+            "original_language": "en",
+            "original_title": "Code 8 Part II",
+            "overview": "In a world where superpowered people are heavily policed by robots, an ex-con teams up with a drug lord he despises to protect a teen from a corrupt cop.",
+            "popularity": 684.527,
+            "poster_path": "/hhvMTxlTZtnCOe7YFhod9uz3m37.jpg",
+            "release_date": "2024-02-28",
+            "title": "Code 8 Part II",
+            "video": false,
+            "vote_average": 6.487,
+            "vote_count": 350
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/cu5Qk2QHxOyyMrD3Bq93DxgmJer.jpg",
+            "genre_ids": [
+                28,
+                80
+            ],
+            "id": 1046090,
+            "original_language": "zh",
+            "original_title": "周處除三害",
+            "overview": "The arrogant, third most-wanted criminal in Taiwan, decides to get rid of the top two competitors and crowns himself the most-wanted criminal before dying.",
+            "popularity": 670.252,
+            "poster_path": "/7IJ7F8tX7IAkpUdaGovOBJqORnJ.jpg",
+            "release_date": "2024-03-01",
+            "title": "The Pig, the Snake and the Pigeon",
+            "video": false,
+            "vote_average": 7.552,
+            "vote_count": 106
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/yyFc8Iclt2jxPmLztbP617xXllT.jpg",
+            "genre_ids": [
+                35,
+                10751,
+                14
+            ],
+            "id": 787699,
+            "original_language": "en",
+            "original_title": "Wonka",
+            "overview": "Willy Wonka – chock-full of ideas and determined to change the world one delectable bite at a time – is proof that the best things in life begin with a dream, and if you’re lucky enough to meet Willy Wonka, anything is possible.",
+            "popularity": 603.503,
+            "poster_path": "/qhb1qOilapbapxWQn9jtRCMwXJF.jpg",
+            "release_date": "2024-01-31",
+            "title": "Wonka",
+            "video": false,
+            "vote_average": 7.215,
+            "vote_count": 2655
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/bQS43HSLZzMjZkcHJz4fGc7fNdz.jpg",
+            "genre_ids": [
+                878,
+                10749,
+                35
+            ],
+            "id": 792307,
+            "original_language": "en",
+            "original_title": "Poor Things",
+            "overview": "Brought back to life by an unorthodox scientist, a young woman runs off with a debauched lawyer on a whirlwind adventure across the continents. Free from the prejudices of her times, she grows steadfast in her purpose to stand for equality and liberation.",
+            "popularity": 553.457,
+            "poster_path": "/kCGlIMHnOm8JPXq3rXM6c5wMxcT.jpg",
+            "release_date": "2024-03-06",
+            "title": "Poor Things",
+            "video": false,
+            "vote_average": 7.8,
+            "vote_count": 2720
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/4Ep2KivIBUZbkS7kHjW7Qywnhhj.jpg",
+            "genre_ids": [
+                28
+            ],
+            "id": 1049948,
+            "original_language": "en",
+            "original_title": "Vikings: Battle of Heirs",
+            "overview": "A young Viking must come to terms with the realization that he may be the King's son, who was switched at birth, but not before others try to take his rightful place.",
+            "popularity": 624.098,
+            "poster_path": "/87goLbbqrJqAxqDS5cBsik1zKT.jpg",
+            "release_date": "2023-04-28",
+            "title": "Vikings: Battle of Heirs",
+            "video": false,
+            "vote_average": 7.4,
+            "vote_count": 9
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/3JhQQFidqbrD9k9DxGFCba46EFk.jpg",
+            "genre_ids": [
+                14,
+                12,
+                35
+            ],
+            "id": 967847,
+            "original_language": "en",
+            "original_title": "Ghostbusters: Frozen Empire",
+            "overview": "The Spengler family returns to where it all started – the iconic New York City firehouse – to team up with the original Ghostbusters, who've developed a top-secret research lab to take busting ghosts to the next level. But when the discovery of an ancient artifact unleashes an evil force, Ghostbusters new and old must join forces to protect their home and save the world from a second Ice Age.",
+            "popularity": 495.169,
+            "poster_path": "/r65fWcFELCSeyyrkq5wY09EPSGN.jpg",
+            "release_date": "2024-04-17",
+            "title": "Ghostbusters: Frozen Empire",
+            "video": false,
+            "vote_average": 6.9,
+            "vote_count": 96
+        }
+    ],
+    "total_pages": 43236,
+    "total_results": 864706
+}
